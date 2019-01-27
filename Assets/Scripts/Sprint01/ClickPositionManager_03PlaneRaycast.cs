d7315679fd4bbdd05e93320790f43c2cbc360965
enersesian <en6@njit.edu>
2019-01-26 21:00:15 -0500

    continued work on sprint 01 week 01 with PlaneRaycast script exercise.

---
+++ Assets/Scripts/Sprint01/ClickPositionManager_03PlaneRaycast.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickPositionManager_03PlaneRaycast : MonoBehaviour {
+
+    //dont need the layer mask and switched variable declaration to global
+    private GameObject primitive;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0) || Input.GetMouseButton(1))
+        {
+            //Goal: log click position in world space to the console
+
+            //create a vector to store the mouse position
+            //Set it to a default value whose value will never be recorded from the mouse position
+            //so when start clicking will get results of (-1,-1,-1) that we know if not real information
+            Vector3 clickPosition = -Vector3.one;
+
+            //method 3: Raycast using Plane
+            Plane plane = new Plane(Vector3.up, 0f);
+            //Plane plane = new Plane(Vector3.forward, 0f);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distanceToPlane;
+
+            if(plane.Raycast(ray, out distanceToPlane))
+            {
+                clickPosition = ray.GetPoint(distanceToPlane);
+            }
+
+            //print out the position and spawn a sphere
+
+            Debug.Log(clickPosition);
+            primitive = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            primitive.transform.position = clickPosition;
+        }
+	}
+}
