9d2f839718e4c4b91f43806f8048f5bf2319a6b6
enersesian <en6@njit.edu>
2019-02-21 10:39:37 -0500

    Created clock prefab for sprint 2 and started connecting it to daylight system

--- Assets/Scripts/CodingTutorials/Clock.cs
+++ Assets/Scripts/CodingTutorials/Clock.cs
@@ -7,18 +7,23 @@ public class Clock : MonoBehaviour
 {
 
     public Transform hoursTransform, minutesTransform, secondsTransform;
-    const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f;
+    const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f, degreesPerDayCycle = 15f;
     private DateTime timeDiscrete;
     private TimeSpan timeContinuous;
-    public bool continuous;
+    public bool continuous, realTime = true;
+    public Transform dayCycle;
+    [Range(0,24)]
+    public int hours;
 
 	// Use this for initialization
 	void Awake ()
     {
         timeDiscrete = DateTime.Now;
+        Debug.Log(DateTime.Now.TimeOfDay.Hours);
         hoursTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Hour * degreesPerHour, 0f);
         minutesTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Minute * degreesPerMinute, 0f);
         secondsTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Second * degreesPerSecond, 0f);
+        dayCycle.localRotation = Quaternion.Euler(timeDiscrete.TimeOfDay.Hours * degreesPerDayCycle, 0f, 0f);
     }
 
     private void Update()
@@ -26,6 +31,14 @@ public class Clock : MonoBehaviour
         timeDiscrete = DateTime.Now;
         hoursTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Hour * degreesPerHour, 0f);
         minutesTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Minute * degreesPerMinute, 0f);
+        if (realTime)
+        {
+            dayCycle.localRotation = Quaternion.Euler(timeDiscrete.TimeOfDay.Hours * degreesPerDayCycle, 0f, 0f);
+        }
+        else
+        {
+            dayCycle.localRotation = Quaternion.Euler(hours * degreesPerDayCycle, 0f, 0f);
+        }
 
         if (continuous)
         {
