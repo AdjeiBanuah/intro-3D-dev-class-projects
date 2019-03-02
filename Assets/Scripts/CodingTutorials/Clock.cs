1d35e8eb3347c16d103c4a7fce067767ba0d6c16
enersesian <en6@njit.edu>
2019-03-01 20:12:43 -0500

    Updated clock script to allow dev to change daylight manually.

--- Assets/Scripts/CodingTutorials/Clock.cs
+++ Assets/Scripts/CodingTutorials/Clock.cs
@@ -12,8 +12,14 @@ public class Clock : MonoBehaviour
     private TimeSpan timeContinuous;
     public bool continuous, realTime = true;
     public Transform dayCycle;
-    [Range(0,24)]
+    [Range(0,23)]
     public int hours;
+
+    //update hours to float and dayCycle.localRotation to continuous time
+    //update non-realtime to change clock as well
+    //move functionality to dragging on clock hands
+    //set physics layers so dont delete parts of clock with left click
+    //change paint feature to left click so it works on mobile version
 
 	// Use this for initialization
 	void Awake ()
@@ -24,15 +30,17 @@ public class Clock : MonoBehaviour
         minutesTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Minute * degreesPerMinute, 0f);
         secondsTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Second * degreesPerSecond, 0f);
         dayCycle.localRotation = Quaternion.Euler(timeDiscrete.TimeOfDay.Hours * degreesPerDayCycle, 0f, 0f);
+        hours = timeDiscrete.TimeOfDay.Hours;
     }
 
     private void Update()
     {
-        timeDiscrete = DateTime.Now;
-        hoursTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Hour * degreesPerHour, 0f);
-        minutesTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Minute * degreesPerMinute, 0f);
+        timeContinuous = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)timeContinuous.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)timeContinuous.TotalMinutes * degreesPerMinute, 0f);
         if (realTime)
         {
+            hours = timeDiscrete.TimeOfDay.Hours;
             dayCycle.localRotation = Quaternion.Euler(timeDiscrete.TimeOfDay.Hours * degreesPerDayCycle, 0f, 0f);
         }
         else
@@ -53,12 +61,12 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void UpdateContinuous ()
     {
-        timeContinuous = DateTime.Now.TimeOfDay;
         secondsTransform.localRotation = Quaternion.Euler(0f, (float)timeContinuous.TotalSeconds * degreesPerSecond, 0f);
     }
 
     void UpdateDiscrete()
     {
+        timeDiscrete = DateTime.Now;
         secondsTransform.localRotation = Quaternion.Euler(0f, timeDiscrete.Second * degreesPerSecond, 0f);
     }
 }
